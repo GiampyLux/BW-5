fff610b35f4851ff2140b7e60b03e01fe13073d8
GiampyLux <giampaolopaladino01@gmail.com>
2024-08-05 09:56:10 +0200

    Aggiungere i file di progetto.

---
+++ BW-5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BW_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
