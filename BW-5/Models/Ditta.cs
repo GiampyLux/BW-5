230d2c1099482dde79e5bcb446ae3e616479625c
mariantoniettaangelini <164540376+mariantoniettaangelini@users.noreply.github.com>
2024-08-06 12:20:12 +0200

    Ennesime modifiche ai model

---
+++ BW-5/Models/Ditta.cs
@@ -0,0 +1,13 @@
+﻿using BW5.Models;
+
+namespace BW_5.Models
+{
+    public class Ditta
+    {
+        public int IdDitta { get; set; }
+        public string Nome { get; set; }
+        public string Contatto { get; set; }
+        public string Indirizzo { get; set; }
+        public IEnumerable<Prodotto> Prodotti { get; set; }
+    }
+}
