23814bc9f38f038b59aabf3ba3fc4c96ab4093e4
NicoloScivittaro <nscivittaro@gmail.com>
2024-08-06 15:02:03 +0200

    .

--- BW-5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BW_5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
